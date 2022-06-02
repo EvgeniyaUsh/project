36fea0ba88638d87c0aee048ae9c3c775a093ea1
evgeniya <evgeniya.ush20@gmail.com>
2022-06-02 23:27:34 +0300

    rename url

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/likes/', PostLikeViewSet.as_view()),
-    path('api/register', RegisterViewSet.as_view()),
+    path('api/registration/', RegisterViewSet.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
