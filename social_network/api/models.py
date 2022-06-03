aa14a0cd7ad714931c446b088df0cc3341a24a97
EvgeniyaUsh <75211823+EvgeniyaUsh@users.noreply.github.com>
2022-06-03 11:15:29 +0300

    Update models.py

--- social_network/api/models.py
+++ social_network/api/models.py
@@ -2,12 +2,6 @@ from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
 
-phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-CHOICES_APPLICATION_SUBJECT = (
-    ('test', 'Тест драйв'),
-    ('question', 'Консультация'),
-)
-
 
 class Post(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user')
